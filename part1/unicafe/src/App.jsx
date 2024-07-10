4174a82c81d6cc208a8af82aeee49ee569e4d5bb
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-07-10 13:10:50 -0300

    Make a statistics component and render conditional
    - It renders the statistics only if they exist.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,7 +3,29 @@ import { useState } from "react";
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
-const Display = ({ elem }) => <div>{elem}</div>;
+
+const Statisticcs = (props) => {
+  if (props.total == 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <div>
+        <br></br>
+        Good:{props.counters.good}
+        <br></br>
+        Neutral:{props.counters.neutral}
+        <br></br>
+        Bad:{props.counters.bad}
+        <br></br>
+        All: {props.total}
+        <br></br>
+        Average: {props.average}
+        <br></br>
+        Positive: {props.positive}%<br></br>
+      </div>
+    );
+  }
+};
 
 const App = () => {
   let [counters, setCounter] = useState({
@@ -11,7 +33,11 @@ const App = () => {
     neutral: 0,
     bad: 0,
   });
-  console.log(counters);
+
+  let total = counters.good + counters.neutral + counters.bad;
+  let average = (counters.good - counters.bad) / 10;
+  let positive = (counters.good / total) * 100;
+
   const feedback = (feedback) => {
     setCounter((counters) => ({
       ...counters,
@@ -21,14 +47,17 @@ const App = () => {
 
   return (
     <div>
-      <Display elem={<h2>give feedback</h2>} />
+      <h2>give feedback</h2>
       <Button onClick={() => feedback("good")} text="good" />
       <Button onClick={() => feedback("neutral")} text="neutral" />
       <Button onClick={() => feedback("bad")} text="bad" />
-      <Display elem={<h2>statisticcs</h2>} />
-      <Display elem={`good: ${counters.good}`} />
-      <Display elem={`neutral: ${counters.neutral}`} />
-      <Display elem={`bad: ${counters.bad}`} />
+      <h2>statisticcs</h2>
+      <Statisticcs
+        counters={counters}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 };
