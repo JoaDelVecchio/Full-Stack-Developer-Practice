0141a3c2ef37e2658ec04dc626f71819a092c0d9
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-07-10 11:52:04 -0300

    Add funcionality to buttons and display accordingly

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,20 +3,32 @@ import { useState } from "react";
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
-
 const Display = ({ elem }) => <div>{elem}</div>;
 
 const App = () => {
+  let [counters, setCounter] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  console.log(counters);
+  const feedback = (feedback) => {
+    setCounter((counters) => ({
+      ...counters,
+      [feedback]: counters[feedback] + 1,
+    }));
+  };
+
   return (
     <div>
       <Display elem={<h2>give feedback</h2>} />
-      <Button onClick={() => console.log("hi")} text="good" />
-      <Button onClick={() => console.log("hi")} text="neutral" />
-      <Button onClick={() => console.log("hi")} text="bad" />
+      <Button onClick={() => feedback("good")} text="good" />
+      <Button onClick={() => feedback("neutral")} text="neutral" />
+      <Button onClick={() => feedback("bad")} text="bad" />
       <Display elem={<h2>statisticcs</h2>} />
-      <Display elem="good 6" />
-      <Display elem="neutral 2" />
-      <Display elem="bad 1" />
+      <Display elem={`good: ${counters.good}`} />
+      <Display elem={`neutral: ${counters.neutral}`} />
+      <Display elem={`bad: ${counters.bad}`} />
     </div>
   );
 };
