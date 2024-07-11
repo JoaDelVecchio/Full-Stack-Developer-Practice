72c5aaa680059afec961720926731b3c13de01b2
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-07-11 11:38:07 -0300

    Add the total of exercises

--- part1/courseInfo/components/Content.jsx
+++ part1/courseInfo/components/Content.jsx
@@ -7,12 +7,19 @@ const Part = ({ part }) => {
     </div>
   );
 };
+const Sum = ({ parts }) => (
+  <h3>
+    total of {parts.reduce((acum, part) => acum + part.exercises, 0)} exercises
+  </h3>
+);
+
 const Content = ({ parts }) => {
   return (
     <div>
       {parts.map((part) => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
+      <Sum parts={parts} />
     </div>
   );
 };
